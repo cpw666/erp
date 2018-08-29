910efc80759fe903ca8252c137facfafaef72e90
cpw <cpwei120@sina.com>
2018-08-30 06:37:59 +0800

    Change Hello.java in dev branch

---
+++ Action.java
@@ -0,0 +1,10 @@
+public class Hello {
+	public static void main(String args[]) {
+		System.out.println("hehe");
+		System.out.println("hehe2");
+		System.out.println("hehe3");
+		System.out.println("hehe3");
+		System.out.println("hehe3");
+		System.out.println("hehe3");
+	}
+}
--- Hello.java
+++ Hello.java
@@ -1,10 +1,6 @@
 public class Hello {
 	public static void main(String args[]) {
-		System.out.println("hehe");
-		System.out.println("hehe2");
-		System.out.println("hehe3");
-		System.out.println("hehe3");
-		System.out.println("hehe3");
-		System.out.println("hehe3");
+		System.out.println();
+		Date date = new Date();
 	}
 }
