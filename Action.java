b9f88626b625fbc2af31f3a364e9eda1a3290bc3
cpw <cpwei120@sina.com>
2018-08-30 17:51:13 +0800

    ADD patch

--- Action.java
+++ Action.java
@@ -1,6 +1,5 @@
 public class Hello {
 	public static void main(String args[]) {
-		private String title;
 		System.out.println("hehe");
 		System.out.println("hehe2");
 		System.out.println("hehe3");
--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 import java.util.*;
-
+dev
 public class Hello {
 	public static void main(String args[]) {
 		System.out.println("hehe3");
@@ -12,7 +12,7 @@ public class Hello {
 System.out.println("hehe3");
 		Admin admin = new Admin();
 		Emp emp = new Emp();
-		System.out.println();
 		Date date = new Date();
+		System.out.println("运动");
 	}
 }
